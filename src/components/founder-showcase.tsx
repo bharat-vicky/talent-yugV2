a0981c49aa02d46096a4df1b7336a8d8afcd4db9
Hridayanand Gupta <bharatvicky90@gmail.com>
2026-09-14 21:05:27 +0530

    Correct Ritu Raj title to Director and COO

--- src/components/founder-showcase.tsx
+++ src/components/founder-showcase.tsx
@@ -39,7 +39,7 @@ const founders = [
   },
   {
     name: "Ritu Raj",
-    role: "Director & CEO, TalentYug",
+    role: "Director & COO, TalentYug",
     position: "-538.537%",
     shortName: "Ritu",
   },
